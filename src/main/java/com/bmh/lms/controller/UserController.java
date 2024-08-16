7a3a03417df54eaeb85c66af9c6d8e8a037873e6
Sayan Dey <sayandey450@gmail.com>
2024-08-16 15:18:57 +0530

    user controller complete

--- src/main/java/com/bmh/lms/controller/UserController.java
+++ src/main/java/com/bmh/lms/controller/UserController.java
@@ -1,22 +1,97 @@
 package com.bmh.lms.controller;
 
+import com.bmh.lms.dto.auth.AuthReqDTO;
+import com.bmh.lms.dto.user.UserReqDTO;
+import com.bmh.lms.dto.user.UserResDTO;
+import com.bmh.lms.service.auth.AuthService;
+import com.bmh.lms.service.user.UserService;
+import jakarta.validation.constraints.Null;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("api/user")
 public class UserController {
-//    @Autowired
-//    private UserService userService;
-//    @PatchMapping
-//    public RegisterResDTO updateUser(@RequestBody UpdateNameReqDTO updateUserDTO) {
-//        userService.updateUser(updateUserDTO.getEmail(), updateUserDTO.getName());
-//        return new RegisterResDTO("Success");
-//    }
-//
-//    @DeleteMapping
-//    public RegisterResDTO deleteUser(@RequestParam String email) {
-//        userService.deleteUser(email);
-//        return new RegisterResDTO("Success");
-//    }
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping()
+    public ResponseEntity<List<UserResDTO>> getAllUser(
+            @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String token
+    ) {
+        Object[] authData = authService.verifyToken(token);
+        if (authData == null || !((Boolean) authData[1]))
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("{username}")
+    public ResponseEntity<UserResDTO> getUser(
+            @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String token,
+            @PathVariable String username
+    ) {
+        Object[] authData = authService.verifyToken(token);
+        if (authData == null || !((Boolean) authData[1]))
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        UserResDTO res = userService.getUserByUsername(username);
+        if(res == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(userService.getUserByUsername(username), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResDTO> saveUser(
+            @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String token,
+            @RequestBody AuthReqDTO req
+    ) {
+        Object[] authData = authService.verifyToken(token);
+        if (authData == null || !((Boolean) authData[1]))
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+
+        try {
+            return new ResponseEntity<>(
+                    userService.saveUser(req, (String) authData[0]),
+                    HttpStatus.OK
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PatchMapping("{username}")
+    public ResponseEntity<UserResDTO> updateUser(
+            @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String token,
+            @PathVariable String username,
+            @RequestBody UserReqDTO req
+    ) {
+        Object[] authData = authService.verifyToken(token);
+        if (authData == null || !((Boolean) authData[1]))
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        UserResDTO res = userService.updateUser(username, req, (String) authData[0]);
+        return res == null ?
+            new ResponseEntity<>(HttpStatus.NOT_FOUND) :
+            new ResponseEntity<>( res, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{username}")
+    public ResponseEntity<Null> deleteUser(
+            @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String token,
+            @PathVariable String username
+    ) {
+        Object[] authData = authService.verifyToken(token);
+        if (authData == null || !((Boolean) authData[1]))
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return userService.deleteUser(username, (String) authData[0]) ?
+                new ResponseEntity<>(HttpStatus.OK) :
+                new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
