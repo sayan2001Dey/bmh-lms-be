6212f4cdf44386a85226581015e3d8dc15333b51
Archisman Tripathi <archismantripathi@gmail.com>
2024-10-15 11:00:00 +0530

    entered parent deed

--- src/main/java/com/bmh/lms/model/ChainDeedData.java
+++ src/main/java/com/bmh/lms/model/ChainDeedData.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,4 +15,6 @@ public class ChainDeedData {
     private String deedType;
 
     private Integer order;
+
+    private List<String> parentDeedIds;
 }
