7e71e74d8534872347ec1ceb625c36fc9a5652f9
Archisman Tripathi <archismantripathi@gmail.com>
2024-11-04 15:46:24 +0530

    bug fix in khatian master

--- src/main/java/com/bmh/lms/service/khatian/KhatianServiceImpl.java
+++ src/main/java/com/bmh/lms/service/khatian/KhatianServiceImpl.java
@@ -5,11 +5,13 @@ import com.bmh.lms.repository.KhatianRepository;
 import com.bmh.lms.service.utils.CommonUtils;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class KhatianServiceImpl implements  KhatianService{
 
     @Autowired
